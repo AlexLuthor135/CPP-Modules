9a08ba0538b8fdee1ebaa309a8be0e2d3e73a0fc
Alexandros Lappas <alappas@c2r10s6.42wolfsburg.de>
2024-02-18 00:50:49 +0100

    CPP Module 09 ex00 done

--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -16,6 +16,41 @@
 #include <iostream>
 #include <map>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <climits>
+
+
+class BitcoinExchange{
+    private:
+        std::map<std::string, double> _data;
+    public:
+        BitcoinExchange();
+        ~BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange& other);
+        BitcoinExchange&operator=(const BitcoinExchange& other);
+
+        void    InitData();
+        std::map<std::string, double>::iterator   FindData(std::string date);
+
+    class IntException : public std::exception{
+    public:
+        virtual const char* what() const throw();
+};
+
+class NegException : public std::exception{
+    public:
+        virtual const char* what() const throw();
+};
+
+class ConException : public std::exception{
+    public:
+        virtual const char* what() const throw();
+};
+
+};
 
 
 
--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -12,6 +12,55 @@
 
 #include "BitcoinExchange.hpp"
 
+bool	correct_date(std::string date){
+	std::istringstream iss(date);
+	int year, month, day;
+	char dash;
+
+	iss >> year >> dash >> month >> dash >> day;
+	if ((year == 2009 && (month == 1 && day < 2)))
+		return (false);
+	if (year == 2024 && month >=date | value
+2011-01-03 | 3
+2011-01-03 | 2
+2011-01-03 | 1
+2011-01-03 | 1.2
+2011-01-09 | 1
+2012-01-11 | -1
+2001-42-42
+2012-01-11 | 1
+2012-01-11 | 2147483648
+2021-02-29 | 1 2 && day > 15)
+		return (false);
+	if (((year < 2009 || year > 2024) || (month < 1 || month > 12) || (day < 1 || day > 31)))
+		return (false);
+	else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+		return (false);
+	if (month == 2)
+		{
+		if (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+		{
+			if (day > 29)
+				return (false);
+		}
+		else
+		{
+		if (day > 28)
+			return (false);
+		}
+	}
+	return (true);
+	}
+
+std::string trim(std::string str){
+	size_t first = str.find_first_not_of(" \t");
+	if (first == std::string::npos)
+		return ("");
+	size_t last = str.find_last_not_of(" \t");
+	return str.substr(first, last - first + 1);
+	
+}
+
 int	ft_error(int i)
 {
 	if (i == 0)
@@ -22,11 +71,42 @@ int	ft_error(int i)
 }
 
 int main(int argc, char **argv){
-    if (argc != 2)
-        return (ft_error(0));
-    std::ifstream inputFile(argv[1]);
-    if (!inputFile.is_open())
-        return (ft_error(1));
-    
-    
+	if (argc != 2)
+		return (ft_error(0));
+	BitcoinExchange data;
+	try{data.InitData();}
+	catch(std::exception &e){
+		std::cerr << e.what() << std::endl;
+		return (0);}
+	std::ifstream inputFile(argv[1]);
+	if (!inputFile.is_open())
+		return (ft_error(1));
+	std::map<std::string, double> map;
+	std::string line;
+	double value;
+	std::map<std::string, double>::iterator it;
+	while (std::getline(inputFile, line)){
+		std::istringstream iss(line);
+		std::string date;
+		if (std::getline(iss, date, '|')){
+			if (trim(date) == "date")
+				continue ;
+			else if (!correct_date(trim(date)))
+				std::cerr << "Error: bad input => " << date << std::endl;
+			else if (!(iss >> value))
+				std::cerr << "Error: bad value" << std::endl;
+			else if (value < 0)
+				std::cerr << "Error: not a positive number" << std::endl;
+			else if (value > INT_MAX)
+				std::cerr << "Error: too large number" << std::endl;
+			else{
+				date = trim(date);
+				it = data.FindData(date);
+				std::cout << date << " => " << value << " = " << value * it->second << std::endl;
+			}
+		}
+		else
+			std::cerr << "Error: bad input => " << date << std::endl;
+	}
+	inputFile.close();
 }
